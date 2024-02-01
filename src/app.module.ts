264563aa6014943d326fa85425b02e9582a39b11
German <german.suvorov134@gmail.com>
2024-02-01 22:48:25 +0300

    feat: instance simple button crud

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,10 @@ import { CompanyModule } from './company/company.module';
 import { СhannelModule } from './сhannel/сhannel.module';
 import { MessageModule } from './message/message.module';
 import { KeyboardModule } from './keyboard/keyboard.module';
+import { ButtonModule } from './button/button.module';
 
 @Module({
-  imports: [DatabaseModule, CompanyModule, СhannelModule, MessageModule, KeyboardModule],
+  imports: [DatabaseModule, CompanyModule, СhannelModule, MessageModule, KeyboardModule, ButtonModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/button/button.controller.spec.ts
@@ -0,0 +1,20 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { ButtonController } from './button.controller';
+import { ButtonService } from './button.service';
+
+describe('ButtonController', () => {
+  let controller: ButtonController;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [ButtonController],
+      providers: [ButtonService],
+    }).compile();
+
+    controller = module.get<ButtonController>(ButtonController);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+});
